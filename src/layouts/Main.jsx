441d24eead0629764cca0e766781ca52565846ad
Samisahariar <samisahariar69@gmail.com>
2024-06-15 01:25:32 -0700

    this is the thrid commit

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+    return (
+        <div>
+            this is the main element so far at this section and we are  trying to rocking it!!!
+        </div>
+    );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './routers/Routers.jsx';
+import { HelmetProvider } from 'react-helmet-async';
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
